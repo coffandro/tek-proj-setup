779f6561ecc3e9fb738b9b02c614bc8907804658
Frida Rosenaa <macrosedk@gmail.com>
2026-08-13 19:15:39 +0200

    Add core functionality

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"os/exec"
 
 	"github.com/tawesoft/golib/v2/dialog"
 )
@@ -43,6 +44,65 @@ func downloadFile(filepath string, url string) (err error) {
 }
 
 func main() {
+	err := downloadFile(".gitignore",
+		"https://github.com/coffandro/tek-proj-setup/releases/download/files/default.gitignore")
+	if err != nil {
+		dialog.Error(err.Error())
+		panic(err.Error())
+	}
 
-	dialog.Alert("Project set up!")
+	err = downloadFile(".gitattributes ",
+		"https://github.com/coffandro/tek-proj-setup/releases/download/files/default.gitattributes")
+	if err != nil {
+		dialog.Error(err.Error())
+		panic(err.Error())
+	}
+
+	dialog.Alert("Project files downloaded! [1/4]")
+
+	err = downloadFile("git_installer.exe ",
+		"https://github.com/git-for-windows/git/releases/download/v2.55.0.windows.4/Git-2.55.0.4-64-bit.exe")
+	if err != nil {
+		dialog.Error(err.Error())
+		panic(err.Error())
+	}
+
+	err = downloadFile("git_lfs_installer.exe ",
+		"https://github.com/git-lfs/git-lfs/releases/download/v3.7.1/git-lfs-windows-v3.7.1.exe")
+	if err != nil {
+		dialog.Error(err.Error())
+		panic(err.Error())
+	}
+
+	dialog.Alert("Git temp files downloaded! [2/4]")
+
+	dialog.Alert("We are about to download and install git, please follow the instructions in the installer(s), leave everything as default.")
+
+	err = exec.Command("./git_installer.exe").Run()
+	if err != nil {
+		dialog.Error(err.Error())
+		panic(err.Error())
+	}
+
+	err = exec.Command("./git_lfs_installer.exe").Run()
+	if err != nil {
+		dialog.Error(err.Error())
+		panic(err.Error())
+	}
+
+	dialog.Alert("Git installed! [3/4]")
+
+	err = exec.Command("git.exe", "init").Run()
+	if err != nil {
+		dialog.Error(err.Error())
+		panic(err.Error())
+	}
+
+	err = exec.Command("git.exe", "lfs", "install").Run()
+	if err != nil {
+		dialog.Error(err.Error())
+		panic(err.Error())
+	}
+
+	dialog.Alert("Project set up! [4/4]")
 }
